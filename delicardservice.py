c626b2ff1cf5a0c5a459526aeb6b9f2a384d7605
florianklu <floriankluge@icloud.com>
2023-04-04 23:56:41 +0200

    API Error handling, new examples in jupyter notebook

--- delicardservice.py
+++ delicardservice.py
@@ -3,6 +3,7 @@ import os
 from datetime import date, timedelta
 from time import time, sleep
 import requests_cache
+from requests.exceptions import HTTPError
 import pandas as pd
 import glob
 
@@ -19,11 +20,18 @@ def scrapeDelicardApi(cardNr: str, password: str, days: int = 90, forceReload = 
         forceReload (default=False): If True, the cache is ignored and fresh data is requested from the server.
     """
 
-    def _getMs() -> str:
+    def getMs() -> str:
         """Returns the current unix time in milliseconds as a string."""
         return str(int(round(time() * 1000)))
     
-    s = requests_cache.CachedSession(cache_name="delicard_api_cache", ignored_parameters=['_'], allowable_codes=(200,201), allowable_methods=('GET', 'POST'), expire_after=24*3600)
+    def checkResponse(response):
+        try: 
+            response.raise_for_status()
+            print(f"{response}")
+        except HTTPError as e:
+            print(f"Error: Request to {e.response.url} failed. Status code: {e.response.status_code}. Check your username and password. Server might also be in maintanace.")
+    
+    s = requests_cache.CachedSession(cache_name="delicard_api_cache", ignored_parameters=['_', 'authToken'], allowable_codes=(200,201), allowable_methods=('GET', 'POST'), expire_after=24*3600)
     
     if forceReload:
         s.cache.clear()
@@ -34,43 +42,58 @@ def scrapeDelicardApi(cardNr: str, password: str, days: int = 90, forceReload = 
                 "XMLHttpRequest"}
     s.auth = ("KASVC", "zKv5yE1LZUmvW29I")  # well, this password is hard coded in the frontend
 
-    # don't know what this does
-    clientRegResponse = s.post("https://kartenservice.studentenwerk-pb.de/TL1/TLA/ClientReg", params={"ClientID": "20", "RegKey": "4G3hQj7E5ivIzbXNlt4Eu8rLvRb3Dy4g", "format": "JSON", "datenformat": "JSON"})
-    print("ClientReg:", clientRegResponse)
-
-    # Texts for the UI describing the attributes from the API Response. The Kartenservice website makes this request, too.
-    textresResponse = s.get("https://kartenservice.studentenwerk-pb.de/TL1/TLM/KASVC/TEXTRES", params={"LangId": "de-DE", "format": "JSON", "_": _getMs()})
-    print("TEXTRES:", textresResponse)
-
-    # Log In to the server, response is a json document containing the session key (authToken)
-    r = s.post("https://kartenservice.studentenwerk-pb.de/TL1/TLM/KASVC/LOGIN", params={"karteNr": cardNr, "format": "JSON", "datenformat": "JSON"}, json={"BenutzerID":cardNr,"Passwort":password})
-    print("LOGIN: ", r)
-    j = json.loads(r.text)
-    authToken = j[0]["authToken"]
-
-    # Fetches information about the autoload feature (Lastschriftmandat)
-    karte = s.get("https://kartenservice.studentenwerk-pb.de/TL1/TLM/KASVC/KARTE", params={"format": "JSON", "authToken": authToken, "karteNr": cardNr, "_": _getMs()})
-    print("KARTE: ", karte)
-
-    sleep(2.8)
-
-    # Fetch purchasing history data
+    # Parameters for purchasing history data
     # Example (only the last 90 days can be fetched from the server):
-    # dateFrom = "01.01.2023"
+    # dateFrom = "02.01.2023"
     # dateTo = "28.02.2023"
     format = "%d.%m.%Y"
-    now = date.today()
-    dateFrom = (now - timedelta(days=days)).strftime(format)
+    now = date.today() + timedelta(days=1)
+    dateFrom = (now - timedelta(days=days-1)).strftime(format)
     dateTo = now.strftime(format)
+    
+    params = {"format": "JSON", "karteNr": cardNr, "datumVon": dateFrom, "datumBis": dateTo}
+    caching_check_transpos = requests_cache.Request("GET", "https://kartenservice.studentenwerk-pb.de/TL1/TLM/KASVC/TRANSPOS", params=params)
+    caching_check_trans = requests_cache.Request("GET", "https://kartenservice.studentenwerk-pb.de/TL1/TLM/KASVC/TRANS", params=params)
+    data_requests_are_cached = s.cache.contains(request=caching_check_transpos) and s.cache.contains(request=caching_check_trans)
+    
+    # if we need data that is not in cache, we have to login on the server first
+    if not data_requests_are_cached:
+        with s.cache_disabled():
+            print("Logging in...")
+            # don't know what this does
+            clientReg = s.post("https://kartenservice.studentenwerk-pb.de/TL1/TLA/ClientReg", params={"ClientID": "20", "RegKey": "4G3hQj7E5ivIzbXNlt4Eu8rLvRb3Dy4g", "format": "JSON", "datenformat": "JSON"})
+            checkResponse(clientReg)
 
+            # Texts for the UI describing the attributes from the API Response. The Kartenservice website makes this request, too.
+            textres = s.get("https://kartenservice.studentenwerk-pb.de/TL1/TLM/KASVC/TEXTRES", params={"LangId": "de-DE", "format": "JSON", "_": getMs()})
+            checkResponse(textres)
+
+            # Log In to the server, response is a json document containing the session key (authToken)
+            login = s.post("https://kartenservice.studentenwerk-pb.de/TL1/TLM/KASVC/LOGIN", params={"karteNr": cardNr, "format": "JSON", "datenformat": "JSON"}, json={"BenutzerID":cardNr,"Passwort":password})
+            checkResponse(login)
+            j = json.loads(login.text)
+            authToken = j[0]["authToken"]
+
+            karte = s.get("https://kartenservice.studentenwerk-pb.de/TL1/TLM/KASVC/KARTE", params={"format": "JSON", "authToken": authToken, "karteNr": cardNr, "_": getMs()})
+            checkResponse(karte)
+    else:
+        print("Data already cached...")
+        authToken = "" # dummy value, won't be send to server
+
+    # Fetches information about the autoload feature (Lastschriftmandat)
+
+
+    sleep(2.3)
+
+    # Purchasing history data
     # Fetch the booking positions from all transactions. A transaction may include multiple positions.
-    transposData = s.get("https://kartenservice.studentenwerk-pb.de/TL1/TLM/KASVC/TRANSPOS", params={"format": "JSON", "authToken": authToken, "karteNr": cardNr, "datumVon": dateFrom, "datumBis": dateTo, "_": _getMs()})
-    print("TRANSPOS: ", transposData)
+    transposData = s.get("https://kartenservice.studentenwerk-pb.de/TL1/TLM/KASVC/TRANSPOS", params={"format": "JSON", "authToken": authToken, "karteNr": cardNr, "datumVon": dateFrom, "datumBis": dateTo, "_": getMs()})
+    checkResponse(transposData)
 
     # Fetch all transactions. Transaction data includes only payment information, nothing about positions.
-    transactionData = s.get("https://kartenservice.studentenwerk-pb.de/TL1/TLM/KASVC/TRANS", params={"format": "JSON", "authToken": authToken, "karteNr": cardNr, "datumVon": dateFrom, "datumBis": dateTo, "_": _getMs()})
-    print("TRANS: ", transactionData)
-
+    transactionData = s.get("https://kartenservice.studentenwerk-pb.de/TL1/TLM/KASVC/TRANS", params={"format": "JSON", "authToken": authToken, "karteNr": cardNr, "datumVon": dateFrom, "datumBis": dateTo, "_": getMs()})
+    checkResponse(transactionData)
+    
     # create a folder for every account and store data in new files
     if not os.path.exists(cardNr):
         os.makedirs(cardNr)
@@ -78,8 +101,10 @@ def scrapeDelicardApi(cardNr: str, password: str, days: int = 90, forceReload = 
         f.write(transposData.text)
     with open(f"{cardNr}/transactions_{now}_{days}.json", "w+", encoding="utf-8") as f:
         f.write(transactionData.text)
-    with open(f"{cardNr}/karte.json", "w+", encoding="utf-8") as f:
-        f.write(karte.text)
+    if not data_requests_are_cached:
+        with open(f"{cardNr}/karte.json", "w+", encoding="utf-8") as f:
+            f.write(karte.text)
+    print("Data download successful!")
 
 def mergeDataDumps(cardNr: int, clean: bool= False) -> dict[str, pd.DataFrame]:
     """
